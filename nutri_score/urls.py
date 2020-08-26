77f1f3f8bbeb3a0f979ec6e9cf26064faec2a034
yasmine1998 <53656355+yasmine1998@users.noreply.github.com>
2020-08-26 23:54:39 +0200

    Add files via upload

---
+++ nutri_score/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from scan import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.welcome, name='welcome'),
+    path('nutri_code/',views.nutri_code,name="nutri_code"),
+    path('scan/',views.scan,name="scan"),
+]
