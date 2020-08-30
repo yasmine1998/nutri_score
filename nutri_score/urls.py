804be7782e36a9fbcc9f2b2214eebb6384bdb852
yasmine1998 <53656355+yasmine1998@users.noreply.github.com>
2020-08-30 16:29:55 +0200

    Update urls.py

--- nutri_score/urls.py
+++ nutri_score/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('nutri_code/',views.nutri_code,name="nutri_code"),
     path('scan/',views.scan,name="scan"),
+    path('nutri_code/code/',views.code,name="code"),
 ]
