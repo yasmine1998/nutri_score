12ce20e4087d5ed757b6d017d80480ba29af1cb4
yasmine1998 <53656355+yasmine1998@users.noreply.github.com>
2020-09-05 16:50:25 +0200

    Update urls.py

--- nutri_score/urls.py
+++ nutri_score/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from scan import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome, name='welcome'),
-    path('nutri_code/',views.nutri_code,name="nutri_code"),
     path('scan/',views.scan,name="scan"),
-    path('nutri_code/code/',views.code,name="code"),
+    path('nutri_codes/',views.code,name="code"),
 ]
